93e6c3adce3b7148ffe67b8f7705a520b594c10d
HassanAbdulJabbarr <hassan.jabbar@devsinc.com>
2024-07-03 21:33:43 +0500

    Code for normal user Auth on Asp.Net MVC Framework project

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace AuthProject.Controllers
+{
+    [Authorize]
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,67 @@
+﻿using AuthProject.Models;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace AuthProject.Controllers
+{
+    [HandleError]
+    public class LoginController : Controller
+    {
+        LoginDbEntities db = new LoginDbEntities();
+        // GET: Login
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Index( user u, string ReturnUrl )
+        {
+
+            if (IsValid( u ) == true)
+            {
+                FormsAuthentication.SetAuthCookie( u.username, false );
+                Session["username"] = u.username.ToString();
+
+                if (ReturnUrl != null)
+                {
+                    return Redirect( ReturnUrl );
+                }
+                else
+                {
+                    return RedirectToAction( "Index", "Home" );
+                }
+            }
+
+            else
+            {
+                return View();
+            }
+
+        }
+        public bool IsValid( user u )
+        {
+            var credentials = db.users.Where( model => model.username == u.username && model.password == u.password ).FirstOrDefault();
+            if (credentials != null)
+            {
+                return (u.username == credentials.username && u.password == credentials.password);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            Session["username"] = null;
+            return RedirectToAction( "Contact", "Default" );
+
+        }
+
+    }
+}
